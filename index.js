fb9ee58e8c38099cac0687e9fa960182c40fa972
jongeunShin95 <jongeunshin95@gmail.com>
2020-11-10 01:08:28 +0900

    Update 2/2/MongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jongeun:1234@goorm-boiler-plate.7xrqo.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`))
