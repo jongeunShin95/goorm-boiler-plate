765e121f34c03714cc2f3632271c39f3a7f5203c
jongeunShin95 <jongeunshin95@gmail.com>
2020-12-22 00:03:06 +0900

    Update 3/1/BodyParser & PostMan & 회원가입

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jongeun:1234@goorm-boiler-plate.7xrqo.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -11,8 +18,16 @@ mongoose.connect('mongodb+srv://jongeun:1234@goorm-boiler-plate.7xrqo.mongodb.ne
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-
-
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        });
+    });
+})
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`))
