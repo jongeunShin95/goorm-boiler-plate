c58bc2509f8c385a60e44a50f78660af631c3c28
jongeunShin95 <jongeunshin95@gmail.com>
2021-01-10 14:20:47 +0900

    Update 3/7/Auth 기능 만들기

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -25,7 +24,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err });
@@ -35,7 +34,7 @@ app.post('/register', (req, res) => {
     });
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
             return res.json({
@@ -55,6 +54,19 @@ app.post('/login', (req, res) => {
         });
 
     })
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
 });
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -58,13 +58,24 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
 
 userSchema.methods.generateToken = function(cb) {
     var user = this;
-
+ 
     var token = jwt.sign(user._id.toHexString(), 'secretToken');
 
     user.token = token;
     user.save(function(err, user) {
         if (err) return cb(err);
         cb(null, user);
+    });
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        });
     });
 };
 
