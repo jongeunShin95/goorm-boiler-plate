50fca0fc593aa2effa8b15cb1c4abdf1b3096e23
jongeunShin95 <jongeunshin95@gmail.com>
2021-01-17 15:13:11 +0900

    Update 3/15/데이터 Flow & Axios

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const config = require('./config/key');
-const { auth } = require('./middleware/auth');
-const { User } = require('./models/User');
+const config = require('./server/config/key');
+const { auth } = require('./server/middleware/auth');
+const { User } = require('./server/models/User');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,7 +22,11 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.get('/api/hello', (req, res) => {
+    res.send("hello");
+});
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
@@ -32,7 +36,7 @@ app.post('/api/users/register', (req, res) => {
             success: true
         });
     });
-})
+});
 
 app.post('/api/users/login', (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
